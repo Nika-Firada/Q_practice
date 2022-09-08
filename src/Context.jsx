0ad3ff658eb82d0a727f47a8dc20ae8eb424a459
Nika-Firada <nika.firada@gmail.com>
2022-09-09 00:53:12 +0400

    useContext, useRef, useReducer

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState, useContext, createContext } from 'react'
 
-const Context = () => {
+const UserContext = createContext([
+  {
+    firstName:"Bob",
+    lastName: 'Bobberson',
+    suffix: 1,
+    email: "boboboboo@example.com"
+  },
+  (obj) => obj
+]);
+
+//level five
+const LevelFive = ({person}) =>{
+  const [user,setUser] = useContext(UserContext);
   return (
-    <div>Context</div>
+    <div>
+      <h5>{user.firstName}</h5>
+      {/* <button onClick={()=>{
+        setUser(Object.assign({}, user, {suffix: user.suffix + 1}))
+      }}>increment</button> */}
+    </div>
+  )
+}
+//level four
+const LevelFour = ({person}) =>{
+  return(
+  <div>
+    <h4>fourth level</h4>
+    <LevelFive person={person}/>
+  </div>
+  )
+}
+//level three
+const LevelThree = ({person}) =>{
+  return(
+  <div>
+    <h3>Third level</h3>
+    <LevelFour person={person}/>
+  </div>
+  )
+}
+//second level
+const LevelTwo = ({person}) =>{
+  return (
+  <div>
+    <h2>second level</h2>
+    <LevelThree person={person}/>
+  </div>
+  )
+}
+// first level
+const Context = () => {
+  const person = {}
+  const userState = useState({
+    firstName: 'James',
+    lastName: 'Jameson',
+    suffix: 1,
+    email: 'james.jons@example.com'
+  })
+
+  return (
+    <UserContext.Provider value={userState}>
+      <h1>first level</h1>
+      <LevelTwo person={person}/>
+    </UserContext.Provider>
   )
 }
 
--- src/Ref.jsx
+++ src/Ref.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Ref = () => {
+  const [stateNum,setStateNum] = useState(0);
+  const numRef = useRef(0)
+
+  function incrementAndDelayLogging(){
+    setStateNum(stateNum + 1);
+    numRef.current++
+    setTimeout(() => alert(`state: ${stateNum} | ref: ${numRef.current}`), 1000)
+  }
+
   return (
-    <div>Ref</div>
+    <div>
+      <h1>useRef Example</h1>
+      <button onClick={incrementAndDelayLogging}>Delay Logging</button>
+      <h4>state: {stateNum}</h4>
+      <h4>ref: {numRef.current}</h4>
+    </div>
   )
 }
 
